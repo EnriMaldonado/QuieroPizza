228d5fce67e70e59b3f8c631af040aa2cd79019a
EnriMaldonado <enriquemaldonado458@gmail.com>
2022-05-29 21:22:21 -0600

    Agregamos una lista de productos

---
+++ QuieroPizza/QuieroPizza.Web/Controllers/Productomodel.cs
@@ -0,0 +1,14 @@
+﻿namespace QuieroPizza.Web.Controllers
+{
+    internal class Productomodel
+    {
+        internal string Descripcion;
+
+        public Productomodel()
+        {
+
+        }
+
+        public int Id { get; internal set; }
+    }
+}
